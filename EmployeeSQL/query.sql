c6d00628c8bbb552dcb91b3329803e388840c3ec
cawthornebr <56853166+cawthornebr@users.noreply.github.com>
2020-02-05 18:36:17 -0700

    finished code

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -5,7 +5,7 @@ LEFT JOIN salaries s
 ON e.emp_no = s.emp_no;
 
 --2. List employees who were hired in 1986.
---SELECT * FROM employees WHERE Contains(hire_date, '1986'); 
+SELECT * FROM employees WHERE hire_date between '1986' AND '1987'
 
 --3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
 SELECT d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name, dm.from_date, dm.to_date
@@ -24,9 +24,25 @@ LEFT JOIN departments d
 ON de.dept_no = d.dept_no;
 
 --5. List all employees whose first name is "Hercules" and last names begin with "B."
+SELECT * FROM employees WHERE last_name LIKE 'B%' AND first_name = 'Hercules'
 
 --6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+LEFT JOIN dep_emp de
+ON e.emp_no = de.emp_no
+LEFT JOIN departments d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
 
 --7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+LEFT JOIN dep_emp de
+ON e.emp_no = de.emp_no
+LEFT JOIN departments d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
 
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT employees.last_name, COUNT(*) FROM employees GROUP BY last_name;
